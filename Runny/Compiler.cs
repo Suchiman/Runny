e3a7c2dfc1b805076e1b1c5ed714b5a98f0d45aa
Suchiman <robinsue@live.de>
2018-05-25 00:05:28 +0200

    Code

---
+++ Runny/Compiler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace Runny
+{
+    public static class Compiler
+    {
+        public static Assembly LoadSource(string source)
+        {
+            var references = new List<MetadataReference>();
+            references.Add(MetadataReference.CreateFromFile(typeof(object).Assembly.Location));
+
+            var compilation = CSharpCompilation.Create("DynamicCode")
+                .WithOptions(new CSharpCompilationOptions(OutputKind.ConsoleApplication))
+                .AddReferences(references)
+                .AddSyntaxTrees(CSharpSyntaxTree.ParseText(source));
+
+            ImmutableArray<Diagnostic> diagnostics = compilation.GetDiagnostics();
+
+            bool error = false;
+            foreach (Diagnostic diag in diagnostics)
+            {
+                switch (diag.Severity)
+                {
+                    case DiagnosticSeverity.Info:
+                        Console.WriteLine(diag.ToString());
+                        break;
+                    case DiagnosticSeverity.Warning:
+                        Console.WriteLine(diag.ToString());
+                        break;
+                    case DiagnosticSeverity.Error:
+                        error = true;
+                        Console.Error.WriteLine(diag.ToString());
+                        break;
+                }
+            }
+
+            if (error)
+            {
+                throw new Exception("Compilation failed");
+            }
+
+            using (var outputAssembly = new MemoryStream())
+            {
+                compilation.Emit(outputAssembly);
+
+                return Assembly.Load(outputAssembly.ToArray());
+            }
+        }
+
+        public static string Format(string source)
+        {
+            var tree = CSharpSyntaxTree.ParseText(source);
+            var root = tree.GetRoot();
+            var normalized = root.NormalizeWhitespace();
+            return normalized.ToString();
+        }
+    }
+}
---
+++ Runny/Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace Runny.Pages
+{
+    public class IndexModel : BlazorComponent
+    {
+        public string Output = "";
+        public string Code = @"using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(""Hello World"");
+    }
+}";
+
+        public void Run()
+        {
+            Output = "";
+            var asm = Compiler.LoadSource(Code);
+            var hasArgs = asm.EntryPoint.GetParameters().Length > 0;
+
+            var currentOut = Console.Out;
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Exception exception = null;
+            try
+            {
+                asm.EntryPoint.Invoke(null, hasArgs ? new string[][] { null } : null);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            Output = writer.ToString();
+            if (exception != null)
+            {
+                Output += "\r\n" + exception.ToString();
+            }
+            Console.SetOut(currentOut);
+        }
+    }
+}
